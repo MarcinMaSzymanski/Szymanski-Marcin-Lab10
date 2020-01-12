cce460e0302bfbad9c418b4f6b738a5c8f4006aa
Marcin Szymański <mszymanski@student.agh.edu.pl>
2020-01-12 18:13:11 +0100

    Add project files.

---
+++ Lab10/Cw7.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Lab10
+{
+    class Cw7
+    {
+        static int suma = 0;
+        static int count = 1;
+        static int nr_watku = 1;
+        static bool done = false;
+
+        static readonly object m_SyncObject = new object();
+
+        public static void program()
+        {
+            for(int x = 0; x<5; x++)
+            {
+                new Thread(Run).Start();
+            }
+        }
+
+        static private int[][] randomMatrix()
+        {
+            int[][] matrix = new int[3][];
+
+            Random rand = new Random();
+
+            for (int i=0; i<matrix.Length; i++)
+            {
+                int[] a = new int[3];
+                for (int j=0; j<a.Length; j++)
+                {
+                    a[j]= rand.Next(1, 20);
+                }
+                matrix[i] = a;
+            }
+
+            return matrix;
+        }
+
+        static void Run()
+        {
+           
+            int[][] matrix = randomMatrix();
+
+            lock(m_SyncObject)
+            {
+                string nazwa = nr_watku.ToString();
+                nr_watku++;
+
+                Console.WriteLine("Macierz w wątku: " + nazwa);
+
+                int s = 0;
+
+                foreach (var el in matrix)
+                {
+                    string text = "";
+                    foreach (var e in el)
+                    {
+                        text = text + e.ToString() + " ";
+                        s = s + e;
+                    }
+                    Console.WriteLine(text);
+                }
+
+                Console.WriteLine("Suma elementów macierzy w wątku: " + nazwa + " wynosi: " + s);
+
+                suma = suma + s;
+
+                if (count == 5)
+                {
+                    Console.WriteLine("Suma wszystkich elementów macierzy wynosi: " + suma);
+                    count = 0;
+                    suma = 0;
+                    nr_watku = 0;
+                }
+                else count++;
+            }
+
+
+        }
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Threading;
+//using UnityEngine;
+
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool repeat = true;
+            while (repeat)
+            {
+                Thread t = new Thread(sync);
+                Console.WriteLine("Podaj numer cwiczenia");
+                string cw = Console.ReadLine();
+                if (cw == "1") Cw1();
+                else if (cw == "2") Cw2();
+                else if (cw == "3") ThreadTest.Cw3();
+                else if (cw == "4") ThreadExample.Cw4_1();
+                else if (cw == "5") ThreadExample1.Cw5();
+                else if (cw == "6") ThreadExample2.Cw6();
+                else if (cw == "7") Cw7.program();
+
+                else
+                {
+                    Console.WriteLine("Podano nieprawidłową wartość!");
+                    continue;
+                }
+
+                t.Start();
+                t.Join();
+                Console.WriteLine("Powtorzyc? (T/N)");
+                string check = Console.ReadLine();
+                if (check == "N") repeat = false;
+
+            }
+
+        }
+
+        static void sync()
+        {
+        }
+
+        static void Cw1()
+        {
+            Thread t = new Thread(Write1); //Uruchom inny wątek
+            t.Start();
+            // Główny wątek.
+            for (int i = 0; i < 1000; i++) Console.Write("0");
+        }
+        //Inny wątek
+        static void Write1()
+        {
+            for (int i = 0; i < 1000; i++) Console.Write("1");
+        }
+
+        static void Cw2()
+        {
+            new Thread(Run).Start(); // Uruchom Run w innym wątku
+            Run(); // Uruchom Run w głownym wątku
+        }
+        static void Run()
+        {
+            // Deklaracja i użycie zmiennej lokalnej - 'cycles'
+            for (int cycles = 0; cycles < 5; cycles++) Console.Write('x');
+        }
+
+        class ThreadTest
+        {
+            bool done;
+            public static void Cw3()
+            {
+                ThreadTest tt = new ThreadTest();
+                new Thread(tt.Run).Start();
+                tt.Run();
+            }
+            // Zauważ, że Run jest teraz metodą instancji
+            void Run()
+            {
+                if (!done) { done = true; Console.WriteLine("Done"); }
+            }
+        }
+
+        class ThreadExample
+        {
+            static bool done; // Pole statyczne
+            public static void Cw4()
+            {
+                new Thread(Run).Start();
+                Run();
+            }
+            static void Run()
+            {
+                if (!done) { done = true; Console.WriteLine("Done"); }
+            }
+
+            public static void Cw4_1()
+            {
+                new Thread(Run_1).Start();
+                Run_1();
+            }
+
+            static void Run_1()
+            {
+                if (!done) { Console.WriteLine("Done"); done = true; }
+            }
+        }
+
+        class ThreadExample1
+        {
+            static bool done; // Pole statyczne
+
+            static readonly object locker = new object();
+
+            public static void Cw5()
+            {
+                new Thread(Run).Start();
+                Run();
+            }
+
+            static void Run()
+            {
+                lock (locker)
+                {
+                    if (!done) { Console.WriteLine("Done"); done = true; }
+                }
+            }
+
+        }
+
+        class ThreadExample2
+        {
+            public static void Cw6()
+            {
+                Thread t = new Thread(Run);
+                t.Start();
+                t.Join();
+                Console.WriteLine("Thread t has ended!");
+            }
+            static void Run()
+            {
+                for (int i = 0; i < 777; i++) Console.Write(":)");
+            }
+        }
+    }
+    
+}
